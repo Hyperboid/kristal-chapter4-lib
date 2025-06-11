3f1674083277aa95447d04485fb8ad2fed0c0807
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-06-11 03:55:53 -0400

    Use container in prophecy event

--- scripts/world/events/prophecy.lua
+++ scripts/world/events/prophecy.lua
@@ -3,6 +3,8 @@ local event, super = Class(Event, "prophecy")
 
 function event:init(data)
     super.init(self, data)
+    self.container = Object(self.width/2,0)
+    self:addChild(self.container)
     local properties = data and data.properties or {}
     if properties.texture then
         self:setSprite(properties.texture)
@@ -11,7 +13,8 @@ function event:init(data)
     if properties.text then
         self:setText(properties.text)
     end
-    self.container = Object(0,0)
+    self.text_offset_x = properties.toff_x or 0
+    self.text_offset_y = properties.toff_y or 0
 end
 
 function event:getSortPosition()
@@ -21,9 +24,9 @@ end
 function event:setSprite(texture)
     if self.sprite then self.sprite:remove() end
     if not texture then return end
-    self.sprite = Sprite(texture, self.width/2, 0)
+    self.sprite = Sprite(texture)
     self.sprite:setOrigin(0.5,1)
-    self:addChild(self.sprite)
+    self.container:addChild(self.sprite)
     self.sprite:setScale(2)
     self.sprite:addFX(ProphecyScrollFX())
     self.sprite:addFX(ProphecyEchoFX())
@@ -32,18 +35,20 @@ end
 function event:setText(text)
     if self.text then self.text:remove() end
     if not text then return end
-    self.text = Text(text, self.width/2, -self.height, {auto_size = true})
+    self.text = Text(text, self.text_offset_x, -self.height, {auto_size = true})
     self.text:setOrigin(0.5, 1)
 
     self.text:addFX(ProphecyScrollFX())
-    self:addChild(self.text)
+    self.container:addChild(self.text)
 end
 
 function event:update()
     super.update(self)
-    if self.sprite then
-        self.sprite.y = Utils.wave(RUNTIME*2, -10, 10)
-        
+    self.container.y = Utils.wave(RUNTIME*2, -10, 10)
+    if self.sprite and self.text then
+        self.text.y = -self.sprite:getScaledHeight() + self.text_offset_y
+    else
+        self.text.y = self.text_offset_y
     end
     Object.startCache()
     if self:collidesWith(self.world.player) then
