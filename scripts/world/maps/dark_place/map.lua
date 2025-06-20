5bd3b3457837074160c88b2a165c1d72adb1b0a2
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-06-19 22:22:35 -0400

    Like some king of gm_construct

--- scripts/world/maps/dark_place/map.lua
+++ scripts/world/maps/dark_place/map.lua
@@ -23,10 +23,12 @@ function map:onFootstep(char, num)
     local x, y = char:getRelativePos(18/2, 72/2)
     -- TODO: I couldn't find the right numbers
     if Input.down("cancel") then
-        RippleEffect:MakeRipple(x,y, 60, nil, 192, 1, 15):applySpeedFrom(char, 0.75)
+        RippleEffect:MakeRipple(x,y, 60, {74/255, 145/255, 246/255}, 192, 1, 15):applySpeedFrom(char, 0.75)
     else
-        RippleEffect:MakeRipple(x,y, 30, nil, 192/2, 1, 8):applySpeedFrom(char, 0.75)
-        RippleEffect:MakeRipple(x,y, 30, nil, 192/3, 1, 8):applySpeedFrom(char, 0.75)
+        -- RippleEffect:MakeRipple(x,y, 30, nil, 192/2, 1, 8):applySpeedFrom(char, 0.75)
+        -- RippleEffect:MakeRipple(x,y, 30, nil, 192/3, 1, 8):applySpeedFrom(char, 0.75)
+        self.world:addChild(RippleEffect(x, y, 30, 192/2, 8, {74/255, 145/255, 246/255})):applySpeedFrom(char, 0.75)
+        self.world:addChild(RippleEffect(x, y, 30, 192/3, 8, {74/255, 145/255, 246/255})):applySpeedFrom(char, 0.75)
     end
 end
 
