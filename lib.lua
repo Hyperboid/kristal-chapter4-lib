c6c17c71501ed4b0f2df59adf6a1f74a2b6e98ea
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-08-03 21:31:18 -0400

    Remove reduced tp mechanic in favor of egnine's own implementation

--- lib.lua
+++ lib.lua
@@ -9,33 +9,6 @@ function lib:init()
     })
 end
 
----@param name PaletteIndex
-function lib:getPaletteColor(name)
-    if Game.battle and Game.battle.encounter and Game.battle.encounter.reduced_tp then
-        if name == "tension_fill" then
-            return Utils.hexToRgb("#0040c0")
-        elseif name == "tension_max" then
-            return Utils.hexToRgb("#0060a0")
-        elseif name == "tension_decrease" then
-            return {0,0,1}
-        elseif name == "tension_back" then
-            return Utils.hexToRgb("#000080")
-        end
-    end
-    -- Assets.stopAndPlaySound("alert")
-end
-
----@param battler PartyBattler
----@param button ActionButton
-function lib:onActionSelect(battler, button)
-    if Game.battle.encounter.reduced_tp then
-        if button.type == "defend" then
-            Game.battle:pushAction("DEFEND", nil, {tp = -2})
-            return true
-        end
-    end
-end
-
 function Ch4Lib.scr_wave(arg0, arg1, speed_seconds, phase)
     local a4 = (arg1 - arg0) * 0.5;
     return arg0 + a4 + (math.sin((((Kristal.getTime()) + (speed_seconds * phase)) / speed_seconds) * (2 * math.pi)) * a4);
