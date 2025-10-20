Hyperboid/kristal-chapter4-lib

commit bedd854b477f36847394f1c17d56a3a121e46210

Climbing accuracy improvements, new events, and lots more
Co-Authored-By: <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>

scripts/world/events/climbentry.lua
@@ -6,52 +6,6 @@ function event:init(data)
     local properties = data and data.properties or {}
     self.up = properties.up or false
     self.yoffset = properties.yoff or (self.up and -5 or (self.height + 40))
-    self.timer = self:addChild(Timer())
-end
-
-function event:update()
-    super.update(self)
-    if
-        not Game.lock_movement
-        and self.world:hasCutscene()
-        and Input.pressed("confirm")
-        and self.world.player.interact_collider[self.world.player.facing]:collidesWith(self)
-    then
-        self:onInteract(self.world.player, self.world.player.facing)
-    end
-end
-
----@param chara Character
-local function jumpTo(chara, ...)
-    chara:jumpTo(...)
-    return function() return not chara.jumping end
-end
-
-local function co_wrap(func)
-    local thread = coroutine.create(func)
-    return function (...)
-        local ok, msg = coroutine.resume(thread, ...)
-        if not ok then
-            COROUTINE_TRACEBACK = debug.traceback(thread)
-            error(msg)
-        end
-    end
-end
-
-function event:startScript(func)
-    local co co = co_wrap(function ()
-        Game.lock_movement = true
-        func({wait = function (t)
-            if type(t) == "number" then
-                self.timer:after(t, co)
-            else
-                self.timer:afterCond(t, co)
-            end
-            coroutine.yield()
-        end})
-        Game.lock_movement = false
-    end)
-    co()
 end
 
 ---@param player Player
@@ -68,21 +22,23 @@ function event:onInteract(player, dir)
         self.world.timer:tween(10/30, mask, {alpha = 0})
     end
 
-    self:startScript(function (scr)
-        local tx = Utils.round(player.x-(self.x+20), 40)+(self.x+20)
-        tx = Utils.clamp(tx, self.x+20, self.x+self.width-20)
-        local ty = Utils.round(self.y, 40)
+    ---@param cutscene WorldCutscene
+    ---@diagnostic disable-next-line: param-type-mismatch
+    self.world:startCutscene(function (cutscene)
+        local tx = MathUtils.roundToMultiple(player.x-(self.x+20), 40)+(self.x+20)
+        tx = MathUtils.clamp(tx, self.x+20, self.x+self.width-20)
+        local ty = MathUtils.roundToMultiple(self.y, 40)
         if dir == "down" then
             ty = ty + 80
         else
-            ty = ty - 40
+            ty = ty
         end
         
         Assets.playSound("wing")
         player.sprite:set("jump_ball")
-        scr.wait(jumpTo(player,tx,ty,10,.5))
+        cutscene:wait(cutscene:jumpTo(player,tx,ty,10,.5))
         player:resetSprite()
-        self.world:detachFollowers()
+        cutscene:detachFollowers()
         Assets.playSound("noise")
         player:setState("CLIMB")
     end)
@@ -90,21 +46,24 @@ end
 
 ---@param player Player
 function event:preClimbEnter(player)
+    if self.world:hasCutscene() then return end
     if player.state_manager.state == "CLIMB" then
         player:setState("WALK")
         local tx, ty = player.x, self.y
         ty = ty + self.yoffset
-        self:startScript(function (scr)
+        ---@param cutscene WorldCutscene
+        ---@diagnostic disable-next-line: param-type-mismatch
+        self.world:startCutscene(function (cutscene)
             Assets.stopAndPlaySound("wing")
             player.sprite:set("jump_ball")
-            scr.wait(jumpTo(player,tx,ty,10,.5))
+            cutscene:wait(cutscene:jumpTo(player,tx,ty,10,.5))
             player:resetSprite()
             Assets.playSound("noise")
             local id = "climb_fade"
             for i,follower in ipairs(self.world.followers) do
                 local mask = follower:getFX(id)
                 if mask then
-                    self.world.timer:tween(10/30, mask, {alpha = 1}, nil, function ()
+                    self.world.timer:tween(8/30, mask, {alpha = 1}, nil, function ()
                         follower:removeFX(mask)
                     end)
                 end
@@ -112,8 +71,8 @@ function event:preClimbEnter(player)
                 follower:setPosition(tx + (i == 1 and -30 or 30), ty + (self.up and 10 or -10))
                 follower:setFacing(player.facing)
             end
-            self.world.player:interpolateFollowers()
-            self.world:attachFollowers()
+            cutscene:interpolateFollowers()
+            cutscene:attachFollowers()
         end)
     end
 end

commit 575843bacba930d18b9b3f551f35ad8c7c062b04

Fix regression in bedd854 accidentally reverting a2ca5d5

scripts/world/events/climbentry.lua
@@ -6,6 +6,52 @@ function event:init(data)
     local properties = data and data.properties or {}
     self.up = properties.up or false
     self.yoffset = properties.yoff or (self.up and -5 or (self.height + 40))
+    self.timer = self:addChild(Timer())
+end
+
+function event:update()
+    super.update(self)
+    if
+        not Game.lock_movement
+        and self.world:hasCutscene()
+        and Input.pressed("confirm")
+        and self.world.player.interact_collider[self.world.player.facing]:collidesWith(self)
+    then
+        self:onInteract(self.world.player, self.world.player.facing)
+    end
+end
+
+---@param chara Character
+local function jumpTo(chara, ...)
+    chara:jumpTo(...)
+    return function() return not chara.jumping end
+end
+
+local function co_wrap(func)
+    local thread = coroutine.create(func)
+    return function (...)
+        local ok, msg = coroutine.resume(thread, ...)
+        if not ok then
+            COROUTINE_TRACEBACK = debug.traceback(thread)
+            error(msg)
+        end
+    end
+end
+
+function event:startScript(func)
+    local co co = co_wrap(function ()
+        Game.lock_movement = true
+        func({wait = function (t)
+            if type(t) == "number" then
+                self.timer:after(t, co)
+            else
+                self.timer:afterCond(t, co)
+            end
+            coroutine.yield()
+        end})
+        Game.lock_movement = false
+    end)
+    co()
 end
 
 ---@param player Player
@@ -22,23 +68,21 @@ function event:onInteract(player, dir)
         self.world.timer:tween(10/30, mask, {alpha = 0})
     end
 
-    ---@param cutscene WorldCutscene
-    ---@diagnostic disable-next-line: param-type-mismatch
-    self.world:startCutscene(function (cutscene)
-        local tx = MathUtils.roundToMultiple(player.x-(self.x+20), 40)+(self.x+20)
-        tx = MathUtils.clamp(tx, self.x+20, self.x+self.width-20)
-        local ty = MathUtils.roundToMultiple(self.y, 40)
+    self:startScript(function (scr)
+        local tx = Utils.round(player.x-(self.x+20), 40)+(self.x+20)
+        tx = Utils.clamp(tx, self.x+20, self.x+self.width-20)
+        local ty = Utils.round(self.y, 40)
         if dir == "down" then
             ty = ty + 80
         else
-            ty = ty
+            ty = ty - 40
         end
         
         Assets.playSound("wing")
         player.sprite:set("jump_ball")
-        cutscene:wait(cutscene:jumpTo(player,tx,ty,10,.5))
+        scr.wait(jumpTo(player,tx,ty,10,.5))
         player:resetSprite()
-        cutscene:detachFollowers()
+        self.world:detachFollowers()
         Assets.playSound("noise")
         player:setState("CLIMB")
     end)
@@ -46,24 +90,21 @@ end
 
 ---@param player Player
 function event:preClimbEnter(player)
-    if self.world:hasCutscene() then return end
     if player.state_manager.state == "CLIMB" then
         player:setState("WALK")
         local tx, ty = player.x, self.y
         ty = ty + self.yoffset
-        ---@param cutscene WorldCutscene
-        ---@diagnostic disable-next-line: param-type-mismatch
-        self.world:startCutscene(function (cutscene)
+        self:startScript(function (scr)
             Assets.stopAndPlaySound("wing")
             player.sprite:set("jump_ball")
-            cutscene:wait(cutscene:jumpTo(player,tx,ty,10,.5))
+            scr.wait(jumpTo(player,tx,ty,10,.5))
             player:resetSprite()
             Assets.playSound("noise")
             local id = "climb_fade"
             for i,follower in ipairs(self.world.followers) do
                 local mask = follower:getFX(id)
                 if mask then
-                    self.world.timer:tween(8/30, mask, {alpha = 1}, nil, function ()
+                    self.world.timer:tween(10/30, mask, {alpha = 1}, nil, function ()
                         follower:removeFX(mask)
                     end)
                 end
@@ -71,8 +112,8 @@ function event:preClimbEnter(player)
                 follower:setPosition(tx + (i == 1 and -30 or 30), ty + (self.up and 10 or -10))
                 follower:setFacing(player.facing)
             end
-            cutscene:interpolateFollowers()
-            cutscene:attachFollowers()
+            self.world.player:interpolateFollowers()
+            self.world:attachFollowers()
         end)
     end
 end