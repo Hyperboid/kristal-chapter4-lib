51f7eee78dc0f0d0bc01a0ea9d7eb9cb62fa8d1f
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-23 23:17:16 -0700

    Oops lol

---
+++ scripts/world/tilesets/light_areas.lua
@@ -0,0 +1,65 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  name = "light_areas",
+  class = "",
+  tilewidth = 188,
+  tileheight = 360,
+  spacing = 0,
+  margin = 0,
+  columns = 0,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 1,
+    height = 1
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 6,
+  tiles = {
+    {
+      id = 6,
+      image = "../../../assets/sprites/tilesets/lightarea/light_area_mask_gradient.png",
+      width = 188,
+      height = 229
+    },
+    {
+      id = 7,
+      image = "../../../assets/sprites/tilesets/lightarea/light_area_mask_window.png",
+      width = 80,
+      height = 360
+    },
+    {
+      id = 8,
+      image = "../../../assets/sprites/tilesets/lightarea/light_area_mask_window_textureless.png",
+      width = 80,
+      height = 360
+    },
+    {
+      id = 9,
+      image = "../../../assets/sprites/tilesets/lightarea/light_area_simple.png",
+      width = 1,
+      height = 1
+    },
+    {
+      id = 10,
+      image = "../../../assets/sprites/tilesets/lightarea/light_area_spot.png",
+      width = 92,
+      height = 131
+    },
+    {
+      id = 11,
+      image = "../../../assets/sprites/tilesets/lightarea/windowtest.png",
+      width = 80,
+      height = 180
+    }
+  }
+}
---
+++ scripts/world/tilesets/light_areas.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="light_areas" tilewidth="188" tileheight="360" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="6">
+  <image width="188" height="229" source="../../../assets/sprites/tilesets/lightarea/light_area_mask_gradient.png"/>
+ </tile>
+ <tile id="7">
+  <image width="80" height="360" source="../../../assets/sprites/tilesets/lightarea/light_area_mask_window.png"/>
+ </tile>
+ <tile id="8">
+  <image width="80" height="360" source="../../../assets/sprites/tilesets/lightarea/light_area_mask_window_textureless.png"/>
+ </tile>
+ <tile id="9">
+  <image width="1" height="1" source="../../../assets/sprites/tilesets/lightarea/light_area_simple.png"/>
+ </tile>
+ <tile id="10">
+  <image width="92" height="131" source="../../../assets/sprites/tilesets/lightarea/light_area_spot.png"/>
+ </tile>
+ <tile id="11">
+  <image width="80" height="180" source="../../../assets/sprites/tilesets/lightarea/windowtest.png"/>
+ </tile>
+</tileset>
