f5c9f428533ef866d3b5352b142729231f51c357
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-23 21:25:37 -0700

    Prophecy updates

--- scripts/world/events/prophecy.lua
+++ scripts/world/events/prophecy.lua
@@ -5,6 +5,8 @@ function Prophecy:init(data)
     super.init(self, data)
     local properties = data and data.properties or {}
 
+	self.always_visible = properties["alwaysvisible"] or false
+	
     self.container_offset_x       = properties["offx"] or 0                  -- x offset of the sprite
     self.container_offset_y       = properties["offy"] or 0                  -- y offset of the sprite
 	
@@ -16,8 +18,8 @@ function Prophecy:init(data)
     self.sprite_offset_y       = properties["spr_offy"] or 0                  -- y offset of the sprite
 
     self.text                  = properties["text"]                           -- the text to display
-    self.text_offset_x         = properties["txt_offx"] or 0                  -- x offset of the text
-    self.text_offset_y         = properties["txt_offy"] or 0                  -- y offset of the text
+    self.text_offset_x         = properties["txt_offx"] or -160               -- x offset of the text
+    self.text_offset_y         = properties["txt_offy"] or -16                -- y offset of the text
 
 	local tex = Assets.getTexture("world/events/prophecy/"..self.texture or "") or nil
 	if tex then
@@ -56,8 +58,91 @@ function Prophecy:init(data)
 	self.panel_active = false
 end
 
+function Prophecy:getRealWidth()
+    local width = 0
+
+    width = width + self.panel.width*2
+    return width
+end
+
+function Prophecy:getRealHeight()
+    local height = 0
+
+    height = height + self.panel.height*2
+    return height
+end
+
 function Prophecy:getSortPosition()
     return self.x,self.y
+end
+
+function Prophecy:breakProphecy(type, silent)
+	local destroytype = type or 0
+	local silent = silent or false
+    local sprites = Assets.getFrames("world/events/prophecy/prophecy_shatter")
+    if #sprites == 0 then return end
+
+    local delaytime = 30/30
+
+    local broken_container = Object(self.x-self.panel_width, self.y-self.panel_height)
+    broken_container:setScaleOrigin(0.5, 0.5)
+    broken_container:setLayer(self:getLayer())
+    self.parent:addChild(broken_container)
+
+    broken_container.timer = Timer()
+    broken_container:addChild(broken_container.timer)
+
+    local timer = broken_container.timer
+    for _, texture in ipairs(sprites) do
+        local s = Sprite(texture)
+        s:setScale(2)
+        broken_container:addChild(s)
+
+		if destroytype == 3 then
+			s.alpha = 0.3;
+			s:setPhysics({
+				speed = 0,
+				friction = 0.5,
+				direction = math.rad(270)
+			})
+			delaytime = 5
+			broken_container.timer:after(((delaytime * MathUtils.random(5)) + 1)/30, function()
+				s:setPhysics({
+					speed = 2,
+					friction = 0,
+					gravity = 0.5 + MathUtils.random(0.1)
+				})
+			end)
+		else
+			s.alpha = 0.8;
+			s:setPhysics({
+				speed = 2,
+				friction = 0.05,
+				direction = math.rad(Utils.random(360))
+			})
+			s:setGraphics({
+				fade_to = 0,
+				fade = 0.01
+			})
+		end
+    end
+
+    broken_container:setScale(
+        self:getRealWidth() / (sprites[1]:getWidth()*2),
+        self:getRealHeight() / (sprites[1]:getHeight()*2)
+    )
+    broken_container.timer:after(120/30, function()
+        broken_container:remove()
+    end)
+
+	if not silent then
+		broken_container.timer:after(2/30, function() Assets.playSound("glassbreak", 0.4, 0.6) end)
+		Assets.playSound("sparkle_glock", 0.5, 0.8)
+		Assets.playSound("sparkle_glock", 0.5, 0.71)
+		Assets.playSound("punchmed", 0.95, 0.7)
+	end
+	
+    self:remove()
 end
 
 function Prophecy:update()
@@ -72,19 +157,25 @@ function Prophecy:update()
     end
 
     Object.startCache()
-    if self:collidesWith(self.world.player) then
-        --self.afx.alpha = MathUtils.approach(self.afx.alpha, 1, DT*4)
-        self.panel_active = true
-        self.panel.panel_alpha = MathUtils.lerp(self.panel.panel_alpha, 1.2, DTMULT*0.1)
-        -- Needed by nth Sanctuary for Deltarune accuracy.
-        -- TODO: Incorporate into the library once church darkness is more fleshed out
-        Kristal.callEvent("updateLightBeams", (1 - (self.panel.panel_alpha / 1.2)))
-    else
-        --self.afx.alpha = MathUtils.approach(self.afx.alpha, 0, DT*2)
-        self.panel.panel_alpha = MathUtils.lerp(self.panel.panel_alpha, 0, DTMULT*0.2)
-		self.panel_active = false
-        Kristal.callEvent("updateLightBeams", (1 - (self.panel.panel_alpha / 1.2)))
-    end
+	if self.always_visible then
+		self.panel_active = true
+		self.panel.panel_alpha = 1.2
+		Kristal.callEvent("updateLightBeams", (1 - (self.panel.panel_alpha / 1.2)))
+	else
+		if self:collidesWith(self.world.player) then
+			--self.afx.alpha = MathUtils.approach(self.afx.alpha, 1, DT*4)
+			self.panel_active = true
+			self.panel.panel_alpha = MathUtils.lerp(self.panel.panel_alpha, 1.2, DTMULT*0.1)
+			-- Needed by nth Sanctuary for Deltarune accuracy.
+			-- TODO: Incorporate into the library once church darkness is more fleshed out
+			Kristal.callEvent("updateLightBeams", (1 - (self.panel.panel_alpha / 1.2)))
+		else
+			--self.afx.alpha = MathUtils.approach(self.afx.alpha, 0, DT*2)
+			self.panel.panel_alpha = MathUtils.lerp(self.panel.panel_alpha, 0, DTMULT*0.2)
+			self.panel_active = false
+			Kristal.callEvent("updateLightBeams", (1 - (self.panel.panel_alpha / 1.2)))
+		end
+	end
     Object.endCache()
 end
 
