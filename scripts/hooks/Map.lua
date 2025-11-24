c564f7806779d4e70f6a1536d0e3d1e093586eac
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-23 23:03:51 -0700

    Unfinished darkness effect

--- scripts/hooks/Map.lua
+++ scripts/hooks/Map.lua
@@ -29,6 +29,14 @@ function Map:loadLayer(layer, depth)
     return super.loadLayer(self, layer, depth)
 end
 
+function Map:createTileObject(data, x, y, width, height)
+    if data.gid then
+        local gid, flip_x, flip_y = TiledUtils.parseTileGid(data.gid)
+        local tileset, tile_id = self:getTileset(gid)
+        return TileObject(tileset, tile_id, x or data.x, y or data.y, width or data.width, height or data.height, math.rad(data.rotation or 0), flip_x, flip_y, data.properties)
+    end
+end
+
 function Map:loadClimbAreas(layer, depth)
     local parent = self.world
 
