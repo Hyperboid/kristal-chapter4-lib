d6538f99bcb05a48667f5c33ee5bf2d064a089e6
Hyperboid <hyperboid@proton.me>
2026-01-30 20:19:42 -0500

    Remove old cyltower system

--- scripts/hooks/Map.lua
+++ scripts/hooks/Map.lua
@@ -5,21 +5,6 @@ function Map:init(world, data)
     super.init(self, world, data)
 end
 
-function Map:loadTiles(layer, depth)
-    if not StringUtils.startsWith(layer.name, "cyltower") then
-        return super.loadTiles(self, layer, depth)
-    end
-    if not self.cyltower then
-        self.cyltower = CylinderTower(self, depth)
-        self.world:addChild(self.cyltower)
-    end
-    self.cyltower:addLayer(layer, depth)
-    table.insert(self.tile_layers, tilelayer)
-    if StringUtils.startsWith(layer.name:lower(), "battleborder") then
-        table.insert(self.battle_borders, tilelayer)
-    end
-end
-
 function Map:loadLayer(layer, depth)
     if layer.type == "objectgroup" then
         if StringUtils.startsWith(layer.name:lower(), "climbareas") then
